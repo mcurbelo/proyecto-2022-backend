7953d667bf69d4662eceb598e2398307c69eec8b
Vitto <43710931+Vittoilcapo@users.noreply.github.com>
2022-09-26 20:50:23 -0300

    Initial commit

---
+++ src/main/java/com/shopnow/shopnow/ShopNowApplication.java
@@ -0,0 +1,13 @@
+package com.shopnow.shopnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopNowApplication.class, args);
+    }
+
+}
