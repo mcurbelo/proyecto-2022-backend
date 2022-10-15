b325402aa0110625e02db6589666a2371bc90e3c
nicosposito <88863067+nicosposito@users.noreply.github.com>
2022-10-15 01:03:46 -0300

    Cambiar estado producto (#9)

--- src/main/java/com/shopnow/shopnow/repository/UsuarioRepository.java
+++ src/main/java/com/shopnow/shopnow/repository/UsuarioRepository.java
@@ -12,6 +12,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
     Optional<Usuario> findByCorreoAndEstado(String correo, EstadoUsuario estado);
 
+
     boolean existsByCorreoAndEstado(String correo, EstadoUsuario estado);
 
     Optional<Usuario> findByIdAndEstado(UUID id, EstadoUsuario estado);
