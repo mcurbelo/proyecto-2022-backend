160701033c14e7dc4477e84a080efa0b2b9b63fa
nicosposito <88863067+nicosposito@users.noreply.github.com>
2022-10-15 01:18:56 -0300

    Busqueda y listado de productos (#10)

--- src/main/java/com/shopnow/shopnow/controller/ProductoController.java
+++ src/main/java/com/shopnow/shopnow/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.shopnow.shopnow.controller;
 
 import com.shopnow.shopnow.controller.responsetypes.Excepcion;
 import com.shopnow.shopnow.model.datatypes.DtAltaProducto;
+import com.shopnow.shopnow.model.datatypes.DtFiltros;
 import com.shopnow.shopnow.model.datatypes.DtProducto;
 import com.shopnow.shopnow.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -29,6 +31,17 @@ public class ProductoController {
         }
         productoService.agregarProducto(datos, imagenes);
         return new ResponseEntity<>("Producto agregado con exito!!!", HttpStatus.OK);
+    }
+
+    @GetMapping()
+    //Ejemplo http://localhost:8080/api/productos?pageSize=5&pageNo=1&sortBy=title&sortDir=asc
+    public Map<String, Object> busquedaDeProductos(
+            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = "20", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "nombre", required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir,
+            @RequestBody DtFiltros filtros) {
+        return productoService.busquedaDeProductos(pageNo, pageSize, sortBy, sortDir, filtros);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/shopnow/shopnow/service/ProductoService.java
+++ src/main/java/com/shopnow/shopnow/service/ProductoService.java
@@ -3,19 +3,29 @@ package com.shopnow.shopnow.service;
 import com.shopnow.shopnow.controller.responsetypes.Excepcion;
 import com.shopnow.shopnow.model.*;
 import com.shopnow.shopnow.model.datatypes.DtAltaProducto;
+
+import com.shopnow.shopnow.model.datatypes.DtEventoInfo;
+import com.shopnow.shopnow.model.datatypes.DtFiltros;
+import com.shopnow.shopnow.model.datatypes.DtProductoSlim;
 import com.shopnow.shopnow.model.datatypes.DtProducto;
 import com.shopnow.shopnow.model.enumerados.EstadoProducto;
 import com.shopnow.shopnow.model.enumerados.EstadoSolicitud;
 import com.shopnow.shopnow.model.enumerados.EstadoUsuario;
 import com.shopnow.shopnow.repository.CategoriaRepository;
+import com.shopnow.shopnow.repository.EventoPromocionalRepository;
 import com.shopnow.shopnow.repository.ProductoRepository;
 import com.shopnow.shopnow.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductoService {
@@ -31,6 +41,9 @@ public class ProductoService {
 
     @Autowired
     ProductoRepository productoRepository;
+
+    @Autowired
+    EventoPromocionalRepository eventoPromocionalRepository;
 
     public void agregarProducto(DtAltaProducto datosProducto, MultipartFile[] imagenes) throws Excepcion, IOException {
         //TODO Para testear
@@ -88,6 +101,112 @@ public class ProductoService {
         usuarioRepository.save(usuario);
     }
 
+
+    public Map<String, Object> busquedaDeProductos(int pageNo, int pageSize, String sortBy, String sortDir, DtFiltros filtros) {
+
+        if (!sortBy.matches("nombre|fechaInicio|precio|permiteEnvio")) {
+            throw new Excepcion("Atributo de ordenamiento invalido");
+        }
+
+        EventoPromocional evento = new EventoPromocional();
+        if (filtros.getIdEventoPromocional() != null) {
+            Optional<EventoPromocional> resevento = eventoPromocionalRepository.findById(filtros.getIdEventoPromocional());
+            if (resevento.isEmpty())
+                throw new Excepcion("El id del evento no existe");
+            else
+                evento = resevento.get();
+            if (evento.getFechaFin().before(new Date()) || evento.getFechaInicio().after(new Date())) { //Si ya termino o si aun no comenzo
+                throw new Excepcion("El evento no esta disponible");
+            }
+        }
+        List<UUID> productosIdEnCategoria = new ArrayList<>();
+        if (filtros.getCategorias() != null) {
+            List<Categoria> categorias = categoriaRepository.findAllById(filtros.getCategorias());
+            List<UUID> productosFiltro = new ArrayList<>();
+            for (Categoria categoria : categorias) {
+                productosFiltro.addAll(categoria.getProductos().keySet());
+            }
+            productosIdEnCategoria = productosFiltro.stream().distinct().collect(Collectors.toList()); //Quitamos repetidos :)
+        }
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
+        // create Pageable instance
+        Page<Producto> productos;
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        List<UUID> productosValidos = productoRepository.productosValidosParaListar(); //Obtengo todos los id's de productos validos
+        List<UUID> productosCumplenFiltro;
+
+        if (filtros.getCategorias() != null && filtros.getNombre() == null && filtros.getIdEventoPromocional() == null) { // 1 0 0
+            productosCumplenFiltro = productosIdEnCategoria;
+
+        } else if (filtros.getCategorias() == null && filtros.getNombre() != null && filtros.getIdEventoPromocional() == null) { // 0 1 0
+            productosCumplenFiltro = productoRepository.findByNombreContaining(filtros.getNombre());
+
+        } else if (filtros.getCategorias() == null && filtros.getNombre() == null && filtros.getIdEventoPromocional() != null) { // 0 0 1
+            productosCumplenFiltro = evento.getProductos().keySet().stream().toList();
+        } else if (filtros.getCategorias() != null && filtros.getNombre() != null && filtros.getIdEventoPromocional() == null) { // 1 1 0
+            List<UUID> productosFiltro2 = productoRepository.findByNombreContaining(filtros.getNombre());
+            productosCumplenFiltro = productosIdEnCategoria.stream()
+                    .distinct()
+                    .filter(productosFiltro2::contains)
+                    .collect(Collectors.toList());
+        } else if (filtros.getCategorias() == null && filtros.getNombre() != null && filtros.getIdEventoPromocional() != null) { // 0 1 1
+            productosCumplenFiltro = productoRepository.buscarProductoEnEventoYporNombre(filtros.getIdEventoPromocional(), filtros.getNombre());
+        } else if (filtros.getCategorias() != null && filtros.getNombre() == null && filtros.getIdEventoPromocional() != null) { // 1 0 1
+            List<UUID> productosFiltro2 = evento.getProductos().keySet().stream().toList();
+            productosCumplenFiltro = productosIdEnCategoria.stream()
+                    .distinct()
+                    .filter(productosFiltro2::contains)
+                    .collect(Collectors.toList());
+        } else if (filtros.getCategorias() != null && filtros.getNombre() != null && filtros.getIdEventoPromocional() != null) { // 1 1 1
+            List<UUID> productosFiltro2 = productoRepository.buscarProductoEnEventoYporNombre(filtros.getIdEventoPromocional(), filtros.getNombre());
+            productosCumplenFiltro = productosIdEnCategoria.stream()
+                    .distinct()
+                    .filter(productosFiltro2::contains)
+                    .collect(Collectors.toList());
+        } else { // 0 0 0
+            productosCumplenFiltro = productosValidos;
+        }
+        if (productosCumplenFiltro != productosValidos) {
+            List<UUID> productosIDParaMostar = productosValidos.stream()
+                    .distinct()
+                    .filter(productosCumplenFiltro::contains).toList();
+            productos = productoRepository.findByIdIn(productosIDParaMostar, pageable);
+        } else {
+            productos = productoRepository.findByIdIn(productosValidos, pageable);
+        }
+
+
+        List<Producto> listaDeProductos = productos.getContent();
+
+        List<DtProductoSlim> content = listaDeProductos.stream().map(this::generarDtProductoSlim).collect(Collectors.toList());
+
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("productos", content);
+        response.put("currentPage", productos.getNumber());
+        response.put("totalItems", productos.getTotalElements());
+        response.put("totalPages", productos.getTotalPages());
+
+        //Si se quiere obtener info de evento activo
+        if (filtros.getRecibirInfoEventoActivo()) {
+            Optional<EventoPromocional> eventoActivo = eventoPromocionalRepository.eventoActivo();
+            if (eventoActivo.isPresent()) {
+                EventoPromocional eventoInfo = eventoActivo.get();
+                response.put("EventoPromocionalActivo", generarDtEventoInfo(eventoInfo));
+            } else {
+                response.put("EventoPromocionalActivo", null);
+            }
+
+        }
+
+        return response;
+
+    }
+
+    
     public DtProducto obtenerProducto(UUID id) {
         Optional<Producto> resultado = productoRepository.findById(id);
         Producto producto;
@@ -130,7 +249,26 @@ public class ProductoService {
         }
         //TODO Descontar el precio si esta en un evento promocional
         return new DtProducto(id, idVendedor, linksImagenes, producto.getNombre(), producto.getDescripcion(), producto.getPrecio(), producto.getPermiteEnvio(), producto.getComentarios(), nombreVendedor, calificacion, usuario.getImagen(), datosVendedor.getLocales());
-    }
 
+    }
+private DtProductoSlim generarDtProductoSlim(Producto producto) {
+        return new DtProductoSlim(producto.getId(), producto.getNombre(), producto.getImagenesURL().get(0).getUrl(), producto.getPrecio());
+        }
+
+
+    private DtEventoInfo generarDtEventoInfo(EventoPromocional evento) {
+        List<DtProductoSlim> infoProductos = new ArrayList<>();
+        Map<UUID, Producto> enEvento = evento.getProductos();
+        if (enEvento.size() > 20) { //La idea no es enviar todos sino un par para hacer una preview de que el evento esta activo
+            List<Producto> prod = new ArrayList<>(enEvento.values());
+            for (int i = 0; i <= 20; i++) {
+                infoProductos.add(generarDtProductoSlim(prod.get(i)));
+            }
+        } else {
+            enEvento.forEach((k, v) -> infoProductos.add(generarDtProductoSlim(v)));
+        }
+
+        return new DtEventoInfo(infoProductos, evento.getId(), evento.getNombre(), evento.getFechaFin(), evento.getCategorias().keySet().stream().toList());
+    }
 }
 
