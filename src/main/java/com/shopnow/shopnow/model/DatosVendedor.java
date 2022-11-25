12c39fa79ce1471aa8741b6aa93e70c3660c7644
nicosposito <nicolas.sposito16@hotmail.com>
2022-11-25 06:11:00 -0300

    Update DatosVendedor.java

--- src/main/java/com/shopnow/shopnow/model/DatosVendedor.java
+++ src/main/java/com/shopnow/shopnow/model/DatosVendedor.java
@@ -35,6 +35,6 @@ public class DatosVendedor {
     @Column(nullable = false)
     private EstadoSolicitud estadoSolicitud;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE})
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private Map<Integer, Direccion> locales = new HashMap<>();
 }
