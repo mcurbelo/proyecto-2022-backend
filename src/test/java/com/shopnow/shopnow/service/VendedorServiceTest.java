d1531bfa98778c94bc9547c7714eaabca713e4f1
nicosposito <nicolas.sposito16@hotmail.com>
2022-12-03 20:24:32 -0300

    Test Historial ventas

--- src/test/java/com/shopnow/shopnow/service/VendedorServiceTest.java
+++ src/test/java/com/shopnow/shopnow/service/VendedorServiceTest.java
@@ -1,9 +1,9 @@
 package com.shopnow.shopnow.service;
 
-import com.shopnow.shopnow.model.DatosVendedor;
-import com.shopnow.shopnow.model.Generico;
-import com.shopnow.shopnow.model.Producto;
-import com.shopnow.shopnow.model.URLimagen;
+import com.shopnow.shopnow.model.*;
+import com.shopnow.shopnow.model.datatypes.DtCompraSlimVendedor;
+import com.shopnow.shopnow.model.datatypes.DtFiltrosVentas;
+import com.shopnow.shopnow.model.enumerados.EstadoCompra;
 import com.shopnow.shopnow.model.enumerados.EstadoProducto;
 import com.shopnow.shopnow.model.enumerados.EstadoSolicitud;
 import com.shopnow.shopnow.model.enumerados.EstadoUsuario;
@@ -13,9 +13,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
+import java.text.ParseException;
 import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -51,6 +55,8 @@ class VendedorServiceTest {
     private Producto producto1;
     private Producto producto2;
     private Generico vendedor;
+
+    private Generico comprador;
 
     @BeforeEach
     void setUp() {
@@ -93,18 +99,45 @@ class VendedorServiceTest {
         productos.put(producto1.getId(), producto1);
         productos.put(producto2.getId(), producto2);
 
+        Direccion direccion1 = Direccion.builder().id(100).calle("18 de julio").numero("123").localidad("Montevideo").departamento("Montevideo").notas("").build();
+        Map<Integer, Direccion> direccionesEnvio = new HashMap<>();
+
+        CompraProducto infoEntrega = new CompraProducto(888, new Date(), null, true, direccion1, 80.00f, 2, 160.00f, producto1, new ArrayList<>());
+        Compra venta1 = new Compra(UUID.fromString("4e913bb6-73b1-4280-b5dc-af70621c6639"), new Date(), EstadoCompra.Completada, new Tarjeta(), infoEntrega, null, "T1Venta", null);
+        Map<UUID, Compra> ventas = new HashMap<>();
+        ventas.put(venta1.getId(), venta1);
+
         vendedor = Generico.builder().id(UUID.fromString("d652bd18-0d70-4f73-b72f-6627620bc5c8")).fechaNac(new Date())
                 .reclamos(new HashMap<>())
                 .ventas(new HashMap<>())
                 .productos(productos)
                 .webToken("")
                 .estado(EstadoUsuario.Activo)
+                .ventas(ventas)
                 .compras(new HashMap<>())
                 .calificaciones(new HashMap<>())
                 .direccionesEnvio(new HashMap<>())
                 .tarjetas(new HashMap<>())
                 .datosVendedor(new DatosVendedor(0, "EmpresaPrueba", "123123", "87448456654", EstadoSolicitud.Aceptado, new HashMap<>()))
                 .build();
+
+        direccionesEnvio.put(direccion1.getId(), direccion1);
+        Map<String, Tarjeta> tarjetas = new HashMap<>();
+        Map<UUID, Compra> compras = new HashMap<>();
+        compras.put(venta1.getId(), venta1);
+
+        comprador = Generico.builder().id(UUID.fromString("37894b62-ae0a-475f-a425-ffd489effbc1")).fechaNac(new Date())
+                .reclamos(new HashMap<>())
+                .ventas(new HashMap<>())
+                .productos(new HashMap<>())
+                .compras(compras)
+                .correo("comprador@shopnow.com")
+                .braintreeCustomerId("123")
+                .password("aa")
+                .calificaciones(new HashMap<>())
+                .direccionesEnvio(direccionesEnvio)
+                .tarjetas(tarjetas).build();
+
     }
 
     @Test
@@ -118,7 +151,30 @@ class VendedorServiceTest {
     }
 
     @Test
-    void historialVentas() {
+    void historialVentasFiltros() throws ParseException {
+        DtFiltrosVentas filtros = new DtFiltrosVentas("Producto1", new Date(), EstadoCompra.Completada);
+        List<UUID> result = new ArrayList<>();
+        result.add(UUID.fromString("4e913bb6-73b1-4280-b5dc-af70621c6639"));
+        when(compraRepository.ventasPorEstadoYIdusuario(any(), any())).thenReturn(result);
+        when(compraRepository.ventasPorFechaYIdusuario(any(), any())).thenReturn(result);
+        when(compraRepository.ventasPorIdUsuarioYNombreComprador(any(), any())).thenReturn(result);
 
+        Page<Compra> pageResponse = new PageImpl(Arrays.asList(vendedor.getVentas().values().toArray()));
+        when(compraRepository.findByIdIn(any(), any())).thenReturn(pageResponse);
+        when(compraRepository.obtenerComprador(any())).thenReturn(comprador);
+        Map<String, Object> historial = vendedorService.historialVentas(0, 20, "fecha", "asc", filtros, UUID.fromString("d652bd18-0d70-4f73-b72f-6627620bc5c8"));
+
+        List<DtCompraSlimVendedor> ventas = (List<DtCompraSlimVendedor>) historial.get("ventas");
+        assertEquals(1, ventas.size());
+    }
+
+    @Test
+    void historialVentasSinFiltros() throws ParseException {
+        Page<Compra> pageResponse = new PageImpl(Arrays.asList(vendedor.getVentas().values().toArray()));
+        when(compraRepository.ventasPorIdUsuario(any(), any())).thenReturn(pageResponse);
+        when(compraRepository.obtenerComprador(any())).thenReturn(comprador);
+        Map<String, Object> historial = vendedorService.historialVentas(0, 20, "fecha", "asc", null, UUID.fromString("d652bd18-0d70-4f73-b72f-6627620bc5c8"));
+        List<DtCompraSlimVendedor> ventas = (List<DtCompraSlimVendedor>) historial.get("ventas");
+        assertEquals(1, ventas.size());
     }
 }
